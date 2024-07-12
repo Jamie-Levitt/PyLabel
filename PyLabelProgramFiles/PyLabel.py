6358b0a62dd18c46b68cb1b9afec2e226105cad0
Jamie-Levitt <157087206+Jamie-Levitt@users.noreply.github.com>
2024-07-12 14:19:12 +1000

    Update PyLabel.py

--- PyLabelProgramFiles/PyLabel.py
+++ PyLabelProgramFiles/PyLabel.py
@@ -1,16 +1,51 @@
+#!/usr/bin/python3
+
+import argparse, sys
 from typing import Optional
+
+import os, json
+
+if os.sep == '\\':
+    configPath = ('C:\Program Files\PyLabel\config.json')
+elif os.sep == '/':
+    configPath = '/usr/local/PyLabel/config.json'
+
+def loadConfig() -> dict:
+    if os.path.isfile(configPath) is False:
+        os.chdir('C:\Program Files\PyLabel')
+        with open('config.json', 'w') as file:
+            config = {
+                'printer-model': "QL-820NWB",
+                'printer-ip': "192.168.0.51"
+            }
+            json.dump(config, file)
+            file.close()
+    configFile = open(configPath)
+    configData = json.load(configFile)
+
+    config = {
+                'printer-model': configData['printer-model'],
+                'printer-ip': configData['printer-ip']
+    }
+
+    configFile.close()
+    return config
+
+def getConfigSetting(setting:str) -> str:
+    config = loadConfig()
+    return config[setting]
+
+def setConfigSetting(setting:str, value:str):
+    config = loadConfig()
+    config[setting] = value
+
+    with open(configPath, 'w') as configFile:
+        json.dump(config, configFile)
 
 from PIL import Image, ImageDraw, ImageFont
 from brother_ql.conversion import convert
 from brother_ql.backends.helpers import send
 from brother_ql.raster import BrotherQLRaster
-
-import os
-
-if os.sep == '\\':
-    robotoPath = os.path.abspath('Program Files\PyLabel\RobotoRegular.ttf')
-elif os.sep == '/':
-    robotoPath = '/usr/local/PyLabel/RobotoRegular.ttf'
 
 def mm_to_pixels(mm, dpi=300):
     return int(mm * dpi / 25.4)
@@ -45,19 +80,22 @@ def generate_image(text_list:list, image_width_mm:int, spacing_mm:int, aTL: Opti
         text_bbox = draw.textbbox((0, 0), 'NO AUTHORITY TO LEAVE', font=font)
         text_height = text_bbox[3] - text_bbox[1]
         total_height_px += text_height + spacing_px'''
+    
+    current_height_px = 0
 
     for text in text_list:
         max_font_size = get_max_font_size(text, image_width_px, 'arial.ttf', draw)
         font_size = max_font_size if max_font_size < font_size else font_size
-        font = ImageFont.truetype('arial.ttf', max_font_size)
-        text_bbox = draw.textbbox((0, 0), text, font=font, align='left')
+        font = ImageFont.truetype('arial.ttf', font_size)
+        text_bbox = draw.textbbox((0, 0), text, font=font)
         text_height = text_bbox[3] - text_bbox[1]
+        draw.text((5, current_height_px), text, font=font, fill="black", align='left')
+        current_height_px += text_height + spacing_px
+
+    total_height_px = current_height_px
     
-    total_height_px = (text_height + spacing_px) * (len(text_list))
-    print(total_height_px)
-    
-    #image = Image.new("RGB", (image_width_px, total_height_px), color="white")
-    #draw = ImageDraw.Draw(image)
+    image = Image.new("RGB", (image_width_px, total_height_px), color="white")
+    draw = ImageDraw.Draw(image)
 
     '''text =
     for i, item in enumerate(text_list):
@@ -68,7 +106,6 @@ def generate_image(text_list:list, image_width_mm:int, spacing_mm:int, aTL: Opti
     draw.text((0,0), text, font = ImageFont.truetype('arial.ttf', font_size), align='left')'''
     
     current_height_px = 0
-    print("hi")
 
     '''if aTL == False:
         max_font_size = get_max_font_size('NO AUTHORITY TO LEAVE', image_width_px, 'arial.ttf', draw)
@@ -87,8 +124,6 @@ def generate_image(text_list:list, image_width_mm:int, spacing_mm:int, aTL: Opti
     
     if(current_height_px != total_height_px):
         image = 'error - ' + str(current_height_px) + ' | ' + str(total_height_px)
-
-    image = len(text_list)
 
     #image.show()
     #image.save("C:\\Users\\jamie\\Desktop\\img.png")
@@ -117,3 +152,44 @@ def printLabel(pIP: str, pModel: str, lblImage: Image):
     )  
 
     send(instructions=instructions, printer_identifier=printer, backend_identifier=backend, blocking=True)
+
+
+def handle_print(lines:Optional[str], aTL:Optional[bool]):
+    linesList = lines.split(';')
+    img = generate_image(linesList, 62, 2, aTL)
+    printLabel(getConfigSetting('printer-ip'), getConfigSetting('printer-model'), img)
+
+def handle_config(pModel:Optional[str] = None, pIP:Optional[str] = None):
+    if pModel is not None:
+        setConfigSetting('printer-model', pModel)
+        print('Printer Model has been set to ' + getConfigSetting('printer-model'))
+    if pIP is not None:
+        setConfigSetting('printer-ip', pIP)
+        print('Printer IP has been set to ' + getConfigSetting('printer-ip'))
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='command')
+
+    parser_print = subparsers.add_parser('print')
+    parser_print.add_argument('-l', '--lines', type=str, required=True)
+    parser_print.add_argument('-a', '--authorityToLeave')
+
+    parser_config = subparsers.add_parser('config', help='Configure settings')
+    parser_config.add_argument('-m', '--printer_model', type=str)
+    parser_config.add_argument('-i', '--printer_ip', type=str)
+
+    args = parser.parse_args()
+
+    if args.command == 'print':
+        handle_print(args.lines, args.authorityToLeave)
+    elif args.command == 'config':
+        handle_config(pModel = args.printer_model, pIP = args.printer_ip)
+    else:
+        parser.print_help()
+        sys.exit(1)
+    
+    return
+
+if __name__ == '__main__':
+    main()
